f8b84c33374219d920da60bf559902b3fe1d285b
mariobgsp <mariobgsp@users.noreply.github.com>
2020-12-12 18:17:20 +0700

    up

---
+++ flash_sale_bot.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time 
+
+PATH = "D:/bot_gabut/chromedriver.exe" # Please be mind your path directory chromedriver.exe  
+driver = webdriver.Chrome(PATH)
+
+# Login Credentials
+email = "email saya"
+password = "password saya"
+
+# Shopeee HTML
+login_link = "https://shopee.co.id/buyer/login" # Login Page
+
+# Product Link #With Product Link Example
+product_link = "https://shopee.co.id/V8-Full-Hd-Webcam-1080P-With-Microphone-Web-cam-FULL-HD-1080-P-i.61050481.3238894052"
+
+# BOT START 
+driver.get(login_link)
+
+time.sleep(3)
+
+# inputing login credentials
+user = driver.find_element_by_class_name('_56AraZ')
+user.send_keys(email) # input email put it between a string ''
+
+time.sleep(1) # wait to load 3s, if you want it faster, you should reduce the number,
+              # but it depends on your internet speed
+
+pwd = driver.find_element_by_xpath("//input[@name='password']")
+pwd.send_keys(password) # input password put it between string ''
+
+time.sleep(1) # wait to load 3s, if you want it faster, you should reduce the number,
+              # but it depends on your internet speed 
+
+# automatically click login button
+login_button = driver.find_element_by_xpath("//button[@class='_35rr5y _32qX4k _1ShBrl _3z3XZ9 _2iOIqx _2h_2_Y']")
+login_button.click()
+
+time.sleep(10) # 
+
+# Buy buy buy
+driver.get(product_link)
+
+time.sleep(2) # wait to load
+
+buy_out = driver.find_element_by_xpath("//button[@class='btn btn-solid-primary btn--l YtgjXY']")
+buy_out.click()
